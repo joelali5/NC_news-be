4f0ee2840f3a68906e59a5d335c7c2fb6f05f1d0
Joel Aliyu <joelaliyu1@gmail.com>
2022-11-20 14:46:15 +0000

    This commit will refactor the task 5

--- utils/db.js
+++ utils/db.js
@@ -25,12 +25,12 @@ const checkUserExists = (username) => {
 };
 
 const checkTopicExists = (topic) => {
-  return db.query(`SELECT * FROM topics WHERE slug = $1;`, [topic])
+  return db.query(`SELECT * FROM articles WHERE topic = $1;`, [topic])
     .then(result => {
       if(result.rows.length === 0){
         return Promise.reject({
           status: 404,
-          msg: "topic not found!"
+          msg: "Topic not found!"
         });
       };
     });
