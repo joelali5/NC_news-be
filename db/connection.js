6675ab4e8b04cb5271e286e9ba80c73a35a278b7
Joel Aliyu <joelaliyu1@gmail.com>
2022-12-05 10:19:17 +0000

    This commit will make some changes to files

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,10 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+console.log(ENV, process.env.DATABASE_URL)
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 const config =
